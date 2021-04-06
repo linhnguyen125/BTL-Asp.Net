4bd04038bd982759c173a51d48f8899ab44de4ce
linhnguyen125 <linhnguyennd125@gmail.com>
2021-04-06 12:24:42 +0700

    update

--- Models/DAO/UserDAO.cs
+++ Models/DAO/UserDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models.EF;
+using PagedList;
 
 namespace Models.DAO
 {
@@ -32,7 +33,7 @@ namespace Models.DAO
             }
             else
             {
-                if(result.Status == false)
+                if (result.Status == false)
                 {
                     return -1;
                 }
@@ -46,6 +47,54 @@ namespace Models.DAO
         public User GetById(string username)
         {
             return db.Users.SingleOrDefault(x => x.Username == username);
+        }
+
+        public IEnumerable<User> getAll(int page, int pageSize)
+        {
+            return db.Users.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.Users.Find(entity.ID);
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Name = entity.Name;
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.Status = entity.Status;
+                user.CreatedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
--- TheGioiDiDong/Areas/Admin/Controllers/UserController.cs
+++ TheGioiDiDong/Areas/Admin/Controllers/UserController.cs
@@ -6,15 +6,18 @@ using System.Web.Mvc;
 using Models.DAO;
 using Models.EF;
 using TheGioiDiDong.Common;
+using PagedList;
 
 namespace TheGioiDiDong.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
         // GET: Admin/User
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, int pageSize = 10)
         {
-            return View();
+            var userDao = new UserDAO();
+            var model = userDao.getAll(page, pageSize);
+            return View(model);
         }
 
         [HttpGet]
@@ -28,10 +31,10 @@ namespace TheGioiDiDong.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                var dao = new UserDAO();
+                var userDao = new UserDAO();
                 var encryptedMdtPas = Encryptor.MD5Hash(user.Password);
                 user.Password = encryptedMdtPas;
-                long id = dao.Insert(user);
+                long id = userDao.Insert(user);
                 if (id > 0)
                 {
                     return RedirectToAction("Index", "User");
@@ -47,6 +50,44 @@ namespace TheGioiDiDong.Areas.Admin.Controllers
         public ActionResult Json()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDAO().ViewDetail(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDao = new UserDAO();
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encryptedMdtPas = Encryptor.MD5Hash(user.Password);
+                    user.Password = encryptedMdtPas;
+                }
+                var result = userDao.Update(user);
+                if (result)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm user không thành công");
+                }
+            }
+            return View("Create");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var userDao = new UserDAO();
+            userDao.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
